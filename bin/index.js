ea64a3d28b67c6eef13308b766f9d2dd0767d1e5
imfly <kubying@foxmail.com>
2016-09-01 07:48:33 +0800

    Add revert puml to picture

--- bin/index.js
+++ bin/index.js
@@ -8,19 +8,6 @@ var pkg = require("../package.json");
 var config = require('../lib/config.js');
 var Js2uml = require("../lib/");
 
-program
-    .version(pkg.version)
-    .option("-b, --borderColor [string]", "border color, default is `#blue\9932CC`")
-    .option("-f, --footer [string]", "The uml's footer description.")
-    .option("-g, --backgroundcolor [string]", "background color, default is `AntiqueWhite/white`")
-    .option("-h, --header [string]", "The uml's header description.")
-    .option("-o, --outputFile [string]", "output file, defaut is `./methodName.pu`")
-    .option("-s, --sourceFile <string>", "sourceFile, required")
-    .option("-t, --title [string]", "The uml's title.")
-    .parse(process.argv);
-
-clone(program, config);
-
 function clone(options, config) {
     for (key in config) {
         if (options[key]) {
@@ -29,9 +16,17 @@ function clone(options, config) {
     }
 }
 
-Js2uml(config);
-if (config.sourceFile) {
+program
+    .version(pkg.version)
+    .option("-b, --borderColor [string]", "border color, default is `#blue\9932CC`")
+    .option("-f, --footer [string]", "The uml's footer description.")
+    .option("-g, --backgroundcolor [string]", "background color, default is `AntiqueWhite/white`")
+    .option("-h, --header [string]", "The uml's header description.")
+    .option("-o, --outputFile [string]", "output file, defaut is `./methodName.pu`")
+    .option("-s, --sourceFile <string>", "sourceFile path, required")
+    .option("-t, --title [string]", "The uml's title.")
+    .parse(process.argv);
 
-}else{
-  
-}
+clone(program, config);
+
+Js2uml(config);
